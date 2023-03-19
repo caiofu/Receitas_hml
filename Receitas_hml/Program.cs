3ad6466464a04d2bdbcc1d182c565addd532b40b
caiofu <caiofu@hotmail.com>
2023-03-19 13:20:07 -0300

    Formulario principal

--- Receitas_hml/Program.cs
+++ Receitas_hml/Program.cs
@@ -11,7 +11,7 @@ namespace Receitas_hml
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
-			Application.Run(new detalhesReceita());
+			Application.Run(new telaPrincipal());
 		}
 	}
 }
--- Receitas_hml/telaPrincipal.Designer.cs
+++ Receitas_hml/telaPrincipal.Designer.cs
@@ -28,19 +28,116 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.SuspendLayout();
-            // 
-            // telaPrincipal
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "telaPrincipal";
-            this.Text = "Receitas";
-            this.ResumeLayout(false);
-
+			textBox1 = new TextBox();
+			panel1 = new Panel();
+			btnBuscarReceita = new Button();
+			groupBox1 = new GroupBox();
+			dataGridView1 = new DataGridView();
+			nomeReceita = new DataGridViewTextBoxColumn();
+			dificuldadeReceita = new DataGridViewTextBoxColumn();
+			receitaAutor = new DataGridViewTextBoxColumn();
+			panel1.SuspendLayout();
+			groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			SuspendLayout();
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(3, 3);
+			textBox1.Multiline = true;
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(457, 33);
+			textBox1.TabIndex = 0;
+			// 
+			// panel1
+			// 
+			panel1.Controls.Add(btnBuscarReceita);
+			panel1.Controls.Add(textBox1);
+			panel1.Location = new Point(21, 12);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(1015, 55);
+			panel1.TabIndex = 1;
+			// 
+			// btnBuscarReceita
+			// 
+			btnBuscarReceita.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			btnBuscarReceita.BackColor = SystemColors.Control;
+			btnBuscarReceita.Font = new Font("Arial Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+			btnBuscarReceita.Location = new Point(496, 3);
+			btnBuscarReceita.Name = "btnBuscarReceita";
+			btnBuscarReceita.Padding = new Padding(5);
+			btnBuscarReceita.Size = new Size(284, 34);
+			btnBuscarReceita.TabIndex = 1;
+			btnBuscarReceita.Text = "Buscar Receita";
+			btnBuscarReceita.UseVisualStyleBackColor = false;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Controls.Add(dataGridView1);
+			groupBox1.Location = new Point(21, 90);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new Size(1015, 462);
+			groupBox1.TabIndex = 2;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Receitas";
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.AllowUserToAddRows = false;
+			dataGridView1.AllowUserToDeleteRows = false;
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nomeReceita, dificuldadeReceita, receitaAutor });
+			dataGridView1.Location = new Point(6, 22);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.ReadOnly = true;
+			dataGridView1.RowTemplate.Height = 25;
+			dataGridView1.Size = new Size(994, 153);
+			dataGridView1.TabIndex = 0;
+			// 
+			// nomeReceita
+			// 
+			nomeReceita.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+			nomeReceita.HeaderText = "Receita";
+			nomeReceita.Name = "nomeReceita";
+			nomeReceita.ReadOnly = true;
+			// 
+			// dificuldadeReceita
+			// 
+			dificuldadeReceita.HeaderText = "Dificuldade";
+			dificuldadeReceita.Name = "dificuldadeReceita";
+			dificuldadeReceita.ReadOnly = true;
+			// 
+			// receitaAutor
+			// 
+			receitaAutor.HeaderText = "Autor";
+			receitaAutor.Name = "receitaAutor";
+			receitaAutor.ReadOnly = true;
+			// 
+			// telaPrincipal
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1057, 564);
+			Controls.Add(groupBox1);
+			Controls.Add(panel1);
+			Name = "telaPrincipal";
+			Text = "Receitas";
+			panel1.ResumeLayout(false);
+			panel1.PerformLayout();
+			groupBox1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			ResumeLayout(false);
 		}
 
 		#endregion
+
+		private TextBox textBox1;
+		private Panel panel1;
+		private Button btnBuscarReceita;
+		private GroupBox groupBox1;
+		private DataGridView dataGridView1;
+		private DataGridViewTextBoxColumn nomeReceita;
+		private DataGridViewTextBoxColumn dificuldadeReceita;
+		private DataGridViewTextBoxColumn receitaAutor;
 	}
 }
