79eab53ebd0143e29914fabeb0c3362d4a6c9de2
KenzoMasuda <108629795+KenzoMasuda@users.noreply.github.com>
2023-03-19 15:21:45 -0300

    Criacao dos atributos e logica da função EditarReceita

--- Receitas_hml/Receita.cs
+++ Receitas_hml/Receita.cs
@@ -8,5 +8,19 @@ namespace Receitas_hml
 {
 	internal class Receita
 	{
+		String nome { get; set; }
+		String imagem { get; set; } 
+		List<String> ingredientes = new List<String>();
+		bool favorito = false; //padrão
+		String dificuldade { get; set; }
+		String categoria { get; set; }
+		List<String> modoPreparo = new List<String>();
+		
+		public void EditarReceita() //botão "Editar" presente em algum Form deverá chamar esta função
+		{ //Não colocarei como argumento um objeto receita, pois ao clicar no botão "Editar",
+		  //a chamada deve procurar a lista de objetos receitas, selecionar a receita e chamar a função, logo poderemos editar os campos com "this.nome" por exemplo
+		  
+			//Chama formulário de edição dos campos do objeto receita, os valores dos txtBox atualizarão os atributos da receita.
+		}
 	}
 }
