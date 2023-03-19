75b66ec726771b05f6e51ab0ca674b693a06a766
KenzoMasuda <108629795+KenzoMasuda@users.noreply.github.com>
2023-03-19 18:55:02 -0300

    Criação de BaseForm, Padronização de verificação de saída, block do redimensionamento de tela

---
+++ Receitas_hml/BaseForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Receitas_hml
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FechamentoFormularioPadrao(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Verificação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
