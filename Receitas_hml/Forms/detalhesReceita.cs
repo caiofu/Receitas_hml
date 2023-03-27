b50e2b82416e1d8067c1c7a73b1aef7fce4de8cd
victorbuquera <victorbuquera@gmail.com>
2023-03-27 20:08:06 -0300

    Criando lista de ingredientes

--- Receitas_hml/Forms/detalhesReceita.cs
+++ Receitas_hml/Forms/detalhesReceita.cs
@@ -10,37 +10,47 @@ using System.Windows.Forms;
 
 namespace Receitas_hml
 {
-	public partial class detalhesReceita : BaseForm
-	{
-		public detalhesReceita()
-		{
-			InitializeComponent();
+    public partial class detalhesReceita : BaseForm
+    {
+        public detalhesReceita()
+        {
+            InitializeComponent();
+
+            Receita receita = new Receita();
             // Criar a lista de itens que deseja adicionar
-            List<string> itens = new List<string>();
-            itens.Add("Item 1");
-            itens.Add("Item 2");
-            itens.Add("Item 3");
+            receita.ingredientes.Add("Ingrediente 0");
+            receita.ingredientes.Add("Ingrediente 1");
+            receita.ingredientes.Add("Ingrediente 2");
+            receita.ingredientes.Add("Ingrediente 3");
+            receita.ingredientes.Add("Ingrediente 4");
+            List<string> ingredientes = new List<string>();
+            ingredientes = receita.ingredientes;
 
 
             // Adicionar itens ao ListView
-            foreach (string item in itens)
+            foreach (string ingrediente in ingredientes)
             {
-                listView1.Items.Add(item);
+                listView1.Items.Add(ingrediente);
             }
 
         }
 
-		private void groupBox2_Enter(object sender, EventArgs e)
-		{
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void label1_Click(object sender, EventArgs e)
-		{
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpDetalhesReceitaCorpo_Enter(object sender, EventArgs e)
         {
 
         }
